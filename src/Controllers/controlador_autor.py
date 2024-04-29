d4454f88955fdceb1a5cd0881699fd156ced21b4
Juandasalazarg <141048830+Juandasalazarg@users.noreply.github.com>
2024-04-29 18:07:04 -0500

    Update controlador_autor.py

--- src/Controllers/controlador_autor.py
+++ src/Controllers/controlador_autor.py
@@ -1,5 +1,3 @@
-from autor import *
-
 class ControladorAutorLibro:
     def _init_(self):
         self.autores = []
@@ -13,3 +11,25 @@ class ControladorAutorLibro:
                 return autor
         return None
 
+    def obtener_autores_por_nacionalidad(self, nacionalidad):
+        autores_nacionalidad = []
+        for autor in self.autores:
+            if autor.nacionalidad == nacionalidad:
+                autores_nacionalidad.append(autor)
+        return autores_nacionalidad
+
+    def eliminar_autor(self, nombre):
+        for autor in self.autores:
+            if autor.nombre == nombre:
+                self.autores.remove(autor)
+                return True
+        return False
+
+    def actualizar_informacion_autor(self, nombre, nueva_nacionalidad, nueva_fecha_nacimiento, nuevo_genero):
+        autor = self.obtener_autor_por_nombre(nombre)
+        if autor:
+            autor.nacionalidad = nueva_nacionalidad
+            autor.fecha_nacimiento = nueva_fecha_nacimiento
+            autor.genero = nuevo_genero
+            return True
+        return False
