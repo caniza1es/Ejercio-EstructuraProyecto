e3e102960441024e60981c92af158e102d6cbe47
Juandasalazarg <141048830+Juandasalazarg@users.noreply.github.com>
2024-04-29 18:11:33 -0500

    Update controlador_autor.py

--- src/Controllers/controlador_autor.py
+++ src/Controllers/controlador_autor.py
@@ -1,3 +1,15 @@
+import psycopg2
+try:
+    connection = psycopg2.connect(
+        host = 'localhost',
+        user = 'postgres',
+        password = '123456789',
+        database = 'Biblioteca'
+    )
+finally:
+    connection.close()
+
+
 class ControladorAutorLibro:
     def _init_(self):
         self.autores = []
